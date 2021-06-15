8a60a49dd07e853a1cdbed141813d8acaff9c7ac
doctormemes <ohwowitsdoc@protonmail.com>
2021-06-15 07:33:06 -0500

    minor fixes and some comments added

--- Image-Resize-script.py
+++ Image-Resize-script.py
@@ -2,13 +2,12 @@ from PIL import Image
 from tkinter import filedialog, Tk
 import os
 
-
 #---withdraw Tkinter and setup choice variables---#
 Tk().withdraw()
 copy_below_size = None
 valid_choices = ["y", "n"]
 
-#---Start value checking phase---#
+#---Start resize value phase---#
 try:
     resize_vals=[int(input("Enter width for resized images: \n")), int(input("Enter height for resized images: \n"))]
 except ValueError:
@@ -16,20 +15,20 @@ except ValueError:
     os.exit()
 if (type(resize_vals[0]) == int and resize_values[0] > 99) and (type(resize_vals[1]) == int and resize_vals[1] > 99):
     new_width, new_height = resize_vals
-else:
+else: 
     print("Error! Image resize values need to be integers greater than 99!\nStopping script...\n")
     os.exit()
-#---End value checking phase---#
+#---End resize value phase---#
 
-#---Start image resize options selection phase---#
-# list  to iterate through files in source directory
-with os.scandir(filedialog.askdirectory(title="Select folder with images to resize")) as path:
-    dest_folder_path = filedialog.askdirectory(title="Select Destination Folder for resized images")
+#---Start selection phase---#
+with os.scandir(filedialog.askdirectory(title="Select Source Folder with images to resize")) as path:
+    dest_folder_path = filedialog.askdirectory(title="Select Destination Folder to save resized images to")
     if dest_folder_path == path:
         print("Error!\nDestination folder is same as source directory! Stopping script...\n")
         os.exit()
+    # find out if user wants to include any images smaller than the resize value
     while True:
-        yes_no = str(input(f"Do you want to copy images in {path} below size 500x500 to {dest_folder_path}?\n(Y/N): ").lower())
+        yes_no = str(input(f"Do you want to copy images in {path} below size {new_width}x{new_height} to {dest_folder_path}?\n(Y/N): ").lower())
         if valid_choices[0] in yes_no:
             copy_below_size = True
             break
@@ -39,11 +38,12 @@ with os.scandir(filedialog.askdirectory(title="Select folder with images to resi
         else:
             print("Please enter a valid choice, this is just a yes or no question.\n")
             continue
+    # list  to iteratethrough files in source directory
     file_list = list()
     for file in path:    
         if file.is_file():
             file_list.append(file.path)
-#---End image resize options selection phase---#
+#---End selection phase---#
 
 #---Start image resizing phase---#
 if copy_below_size:
@@ -58,7 +58,7 @@ if copy_below_size:
             print(f"Original size of {img_name} is {old_width}x{old_height}\n")
             print(f"Resizing {img_name} to {resize_vals}\n")
             # resize image
-            img.thumbnail((new_width. new_height))
+            img.thumbnail((new_width, new_height))
             img.save(f"{dest_folder_path}/{resized_name}")
             print(f"Success! Saved resized image as {resized_name} in directory {dest_folder_path}\n")
 else:
@@ -66,8 +66,8 @@ else:
         try:
             img = Image.open(file)
             old_width, old_height = img.size
-            # anything less than new_width x new_height doesn't need to be resized
-            if old_width <= new_width or old_height <= new_height:
+            # anything less than or equal to new_width x new_height doesn't need to be resized
+            if old_width <= new_width and old_height <= new_height:
                 img_name = os.path.basename(img.filename)
                 split_name = img_name.split('.')
                 # add new img size to save name
@@ -82,5 +82,6 @@ else:
                 print(f"File {img.filename} size is below {new_width}x{new_height}, skipping it...\n")
         except OSError:
             print("OSError exception occurred!")
+#---End image resizing phase---#
 
 print("Operation completed successfully.")
